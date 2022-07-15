5b5a124f803ce482ebc44969aac61dd2e009e527
rajeshmanjunath <rajesh@thisisalice.com>
2022-07-15 07:51:35 +0530

    added dao structure

--- dao/user_dao.go
+++ dao/user_dao.go
@@ -4,18 +4,19 @@ import (
 	"fmt"
 	"net/http"
 
+	"github.com/raj23manj/demo-app-golang/domain"
 	"github.com/raj23manj/demo-app-golang/utils"
 )
 
 type userDao struct{}
 
 type userDaoInterface interface {
-	GetUser(userId int64) (*User, *utils.ApplicationError)
+	GetUser(userId int64) (*domain.User, *utils.ApplicationError)
 }
 
 var (
-	users = map[int64]*User{
-		123: &User{Id: 123, FirstName: "Fede", LastName: "Leon", Email: "myemail@gmail.com"},
+	users = map[int64]*domain.User{
+		123: &domain.User{Id: 123, FirstName: "Fede", LastName: "Leon", Email: "myemail@gmail.com"},
 	}
 
 	UserDao userDaoInterface
@@ -25,7 +26,7 @@ func init() {
 	UserDao = &userDao{}
 }
 
-func (u *userDao) GetUser(userId int64) (*User, *utils.ApplicationError) {
+func (u *userDao) GetUser(userId int64) (*domain.User, *utils.ApplicationError) {
 	user := users[userId]
 	if user == nil {
 		return nil, &utils.ApplicationError{
